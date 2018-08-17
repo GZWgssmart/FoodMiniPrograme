a5bbf2d02439fcfe87d6447aa8703420f49fafab
Wjhsmart <Wjhsmarter@126.com>
2018-08-17 15:24:15 +0800

    删除2.0版vue

--- FoodManProgram-Admin/config/url.js
+++
@@ -1,8 +0,0 @@
-import env from './env'
-
-// 开发环境url地址
-const DEV_URL = 'http://127.0.0.1'
-// 正式环境url地址
-const PRO_URL = 'https://produce.com'
-
-export default env === 'development' ? DEV_URL : PRO_URL
