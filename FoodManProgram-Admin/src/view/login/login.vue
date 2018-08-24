d33d6deaa0ab7884d6ecc477779fb99f31cfe8fb
Wjhsmart <Wjhsmarter@126.com>
2018-08-24 10:39:10 +0800

    解决登入的bug

--- FoodManProgram-Admin/src/view/login/login.vue
+++ FoodManProgram-Admin/src/view/login/login.vue
@@ -1,23 +1,26 @@
 <style lang="less">
-  @import './login.less';
+@import './login.less';
 </style>
 
 <template>
-  <div class="login">
-    <div class="login-con">
-      <Card icon="log-in" title="欢迎登录" :bordered="false">
-        <div class="form-con">
-          <login-form @on-success-valid="handleSubmit"></login-form>
-          <p class="login-tip">输入任意用户名和密码即可</p>
-        </div>
-      </Card>
-    </div>
+<div class="login">
+  <div class="login-con">
+    <Card icon="log-in" title="欢迎登录" :bordered="false">
+      <div class="form-con">
+        <login-form @on-success-valid="handleSubmit"></login-form>
+        <p class="login-tip">输入任意用户名和密码即可</p>
+      </div>
+    </Card>
   </div>
+</div>
 </template>
 
 <script>
 import LoginForm from '_c/login-form'
-import { mapActions } from 'vuex'
+import {
+  mapActions
+} from 'vuex'
+import Cookies from 'js-cookie'
 export default {
   components: {
     LoginForm
@@ -27,7 +30,15 @@ export default {
       'handleLogin',
       'getUserInfo'
     ]),
-    handleSubmit ({ userName, password }) {
+    handleSubmit ({
+      userName,
+      password
+    }) {
+      Cookies.set('user', userName)
+      Cookies.set('password', password)
+      this.$store.commit('avator', '@/assets/images/head.png')
+      Cookies.set('access', 'super_admin')
+      Cookies.set('token', 'super_admin')
       this.$router.push({
         name: 'home'
       })
